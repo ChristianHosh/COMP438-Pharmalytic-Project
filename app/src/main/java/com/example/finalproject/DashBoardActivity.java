6269c220be26e8af8504d3336855413e0734a362
leenimad52 <leenimad52@gmail.com>
2023-06-13 14:56:26 +0300

    1.1 dashBoard(not finished yet)

--- app/src/main/java/com/example/finalproject/DashBoardActivity.java
+++ app/src/main/java/com/example/finalproject/DashBoardActivity.java
@@ -4,13 +4,33 @@ import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.widget.SearchView;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
+
 
 public class DashBoardActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.dash_board);
+        AdapterView.OnItemClickListener itemClickListener = new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent,
+                                    View view,
+                                    int position,
+                                    long id) {
+                if(position == 0){
+                  //  Intent intent = new Intent(DashBoardActivity.this, seconed.class);
+                 //  startActivity(intent);
+                    System.out.println(" ");
+                }
+            }
+        };
+        ListView listView = (ListView)findViewById(R.id.simpleListView);
+        listView.setOnItemClickListener(itemClickListener);
+
 
     }
+
 }
