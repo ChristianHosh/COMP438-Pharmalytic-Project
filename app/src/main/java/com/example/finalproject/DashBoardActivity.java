602957427068f7c69f93efd0e058b53f405bf5b5
leenimad52 <leenimad52@gmail.com>
2023-06-13 14:56:26 +0300

    1.1 dashBoard(not finishe yet)

---
+++ app/src/main/java/com/example/finalproject/DashBoardActivity.java
@@ -0,0 +1,16 @@
+package com.example.finalproject;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.widget.SearchView;
+
+public class DashBoardActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.dash_board);
+
+    }
+}
