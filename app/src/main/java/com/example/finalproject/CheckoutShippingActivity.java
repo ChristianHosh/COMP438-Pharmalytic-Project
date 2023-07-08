69eac6612e504070f93e6e2b6b1d882f575b566e
ChristianHosh <mctibhosh@gmail.com>
2023-07-08 19:22:38 +0300

    created checkout shipping activity

---
+++ app/src/main/java/com/example/finalproject/CheckoutShippingActivity.java
@@ -0,0 +1,14 @@
+package com.example.finalproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CheckoutShippingActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_checkout_shipping);
+    }
+}
